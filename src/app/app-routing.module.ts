d5ae6d78b3ecb9b5569e2600f86d1fde96c26784
names-are-for-losers <132604586+names-are-for-losers@users.noreply.github.com>
2024-11-15 09:22:10 +0400

    I fixed some issues, but I'm still trying to add more pages.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from '.src/app/app.component.html';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { EventsComponent } from './events/events.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'src/app/app.component.html', component: HomeComponent },  // Default route
+  { path: 'src/app/app.login.html', component: SignUpComponent },
+  { path: 'src/app/app.signup.html', component: EventsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
